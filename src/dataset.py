56968d2a807a6e1c3bb9d24ab0637ba27edc61f1
shubhampawar2227 <pawarshubham57243@gmail.com>
2025-04-24 19:00:43 +0530

    New Files Added

---
+++ src/dataset.py
@@ -0,0 +1,15 @@
+# def factorial(number):
+#     if number == 0 or number == 1:
+#         return 1
+#     else:
+#         return number * factorial(number - 1)
+
+
+# factorial(5)
+
+import pandas as pd
+
+df = pd.DataFrame(
+    {"A": [1, 2, 3, 4, 5], "B": [6, 7, 8, 9, 10], "C": [11, 12, 13, 14, 15]}
+)
+df
